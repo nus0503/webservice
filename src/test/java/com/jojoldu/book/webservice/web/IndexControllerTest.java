633ff0514305eedb72d91802d6ed041fbbe64fe1
kjs <nus0503@naver.com>
2023-10-24 00:11:09 +0900

    index.mustache body 출력 테스트

---
+++ src/test/java/com/jojoldu/book/webservice/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.jojoldu.book.webservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        //when
+        String body = this.testRestTemplate.getForObject("/", String.class);
+        System.out.println(body.toString());
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
+}
