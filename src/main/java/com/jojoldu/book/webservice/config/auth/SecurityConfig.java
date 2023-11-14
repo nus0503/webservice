3852fbbf92b7cdd0700ddb1fe0cfb64f0b265325
KJS <nus0503@naver.com>
2023-11-14 16:11:36 +0900

    스프링 시큐리티 + oauth 디펜던시 추가와 시큐리티 설정(미완성)

---
+++ src/main/java/com/jojoldu/book/webservice/config/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.jojoldu.book.webservice.config.auth;
+
+import com.jojoldu.book.webservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Spring Security 설정 활성화
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
