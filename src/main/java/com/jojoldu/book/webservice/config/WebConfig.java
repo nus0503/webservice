a2fa9f5d914fbf07c8275a2d0d1ef657d563015d
kjs <nus0503@naver.com>
2023-11-25 05:04:06 +0900

    다른 컨트롤러와 메소드에서 세견값이 필요할 수도 있기에 메소드 인자로 세션값을 바로 받을 수 있도록 HandlerMethodArgumentResolver를 구현한 클래스를 생성 후 WebMvcConfigurer에 추가

---
+++ src/main/java/com/jojoldu/book/webservice/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.webservice.config;
+
+import com.jojoldu.book.webservice.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer { //스프링에서 인식될 수 있도록
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
---
+++ src/main/java/com/jojoldu/book/webservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.jojoldu.book.webservice.config.auth;
+
+import com.jojoldu.book.webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {//HandlerMethodArgumentResolver은 컨트롤러 메서드에서 특정 조건에 맞는 파라미터가 있을 때 원하는 값을 바인딩해주는 인터페이스입니다.
+
+    private final HttpSession httpSession;
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType()); // 파라미터가 SessionUser타입이면 지원한다.
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, // 파라미터에 전달할 객체를 생성
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
