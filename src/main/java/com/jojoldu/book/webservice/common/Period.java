c94d9ed71932dede78a49a1b38e7945746b2a3ff
kjs <nus0503@naver.com>
2023-12-02 05:20:15 +0900

    공통 Period 생성

---
+++ src/main/java/com/jojoldu/book/webservice/common/Period.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.webservice.common;
+
+import java.time.format.DateTimeFormatter;
+
+public class Period {
+    public static final DateTimeFormatter yyyyMMddHHmm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+}
