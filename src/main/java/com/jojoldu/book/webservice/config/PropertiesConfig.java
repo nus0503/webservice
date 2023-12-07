8620058d6c87a3ce85d6acb1320697b5ec9246e8
KJS <nus0503@naver.com>
2023-12-07 16:54:53 +0900

    another 프로퍼티 파일 생성과 빈 생성

--- src/main/java/com/jojoldu/book/webservice/config/PropertiesConfig.java
+++ src/main/java/com/jojoldu/book/webservice/config/PropertiesConfig.java
@@ -1,19 +1,19 @@
-package com.jojoldu.book.webservice.config;
-
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-@Configuration
-public class PropertiesConfig {
-
-    @Bean(name = "externalApi")
-    public PropertiesFactoryBean propertiesFactoryBean() {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        ClassPathResource classPathResource = new ClassPathResource("properties/externalApi.properties");
-
-        propertiesFactoryBean.setLocation(classPathResource);
-        return propertiesFactoryBean;
-    }
-}
+//package com.jojoldu.book.webservice.config;
+//
+//import org.springframework.beans.factory.config.PropertiesFactoryBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//
+//@Configuration
+//public class PropertiesConfig {
+//
+//    @Bean(name = "externalApi")
+//    public PropertiesFactoryBean propertiesFactoryBean() {
+//        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+//        ClassPathResource classPathResource = new ClassPathResource("externalApi.properties");
+//
+//        propertiesFactoryBean.setLocation(classPathResource);
+//        return propertiesFactoryBean;
+//    }
+//}
