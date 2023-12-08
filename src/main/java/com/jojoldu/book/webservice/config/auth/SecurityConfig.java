4716d2d751b2482e470d7696a38884b799748b7e
kjs <nus0503@naver.com>
2023-12-09 03:43:45 +0900

    시큐리티config 수정

--- src/main/java/com/jojoldu/book/webservice/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/webservice/config/auth/SecurityConfig.java
@@ -27,16 +27,18 @@ public class SecurityConfig extends WebSecurityConfiguration {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/**", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/api/**", "/news/**").permitAll()
+                // antMatchers()메소드는 http의 모든 메소드에 대한 요청을 대상으로 한다. 만약 특정 메소드의 경로를 지정하고 싶으면 .antMatchers(HttpMethod.GET, "경로")
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/api/**", "/news/**", "/signup", "/loginForm", "/user").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated() //위 antMatchers를 제외한 모든 url들은 로그인한 사용자들만 허용한다.
                 .and()
-                    .logout()
+                    .logout() // 로그아웃 경로 기본값은 /logout
                         .logoutSuccessUrl("/")
                 .and()
                 //일반 로그인
                     .formLogin()
                         .loginPage("/loginForm") //로그인 페이지 url
+                        .usernameParameter("email") // 로그인 폼에서 전달하는 파라미터 이름이 시큐리티에서는 기본값으로 username과 password이기에 추가 설정
                         .loginProcessingUrl("/login") // 이 url을 로그인 기능을 담당
                         .defaultSuccessUrl("/") // 로그인 성공시 url
                 .and()
