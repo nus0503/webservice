cb9345bcfde2777144c96cceee95dcc6e4d68441
KJS <nus0503@naver.com>
2023-12-08 19:03:15 +0900

    회원 정보dto

--- src/main/java/com/jojoldu/book/webservice/web/dto/AddUserRequest.java
+++ src/main/java/com/jojoldu/book/webservice/web/dto/AddUserRequest.java
@@ -1,4 +1,12 @@
 package com.jojoldu.book.webservice.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddUserRequest {
+
+    private String email;
+    private String password;
 }
