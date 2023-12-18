1a0a1d3176e7f2c3030fca1c79a989e6147493be
kjs <nus0503@naver.com>
2023-12-19 03:20:22 +0900

    당장 필요없는 코드 주석 -> 확인해서 필요없으면 뺄 것

--- src/main/java/com/jojoldu/book/webservice/web/NoticeController.java
+++ src/main/java/com/jojoldu/book/webservice/web/NoticeController.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.webservice.web;
 
 import com.jojoldu.book.webservice.common.PageableRequest;
+import com.jojoldu.book.webservice.domain.oAuthUser.User;
 import com.jojoldu.book.webservice.service.posts.PostsService;
 import com.jojoldu.book.webservice.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -21,10 +22,10 @@ public class NoticeController {
         return "index";
     }
 
-    @PostConstruct
-    public void test() {
-        for (int i = 0; i < 100; i++) {
-            postsService.save(new PostsSaveRequestDto("title" + i, "content" + i, "a"));
-        }
-    }
+//    @PostConstruct
+//    public void test() {
+//        for (int i = 0; i < 100; i++) {
+//            postsService.save(new PostsSaveRequestDto("title" + i, "content" + i, "a", new User()), "aa");
+//        }
+//    }
 }
