ec2a0490da017ec80c907c190f3e8a79dbecd3ec
KJS <nus0503@naver.com>
2023-12-28 19:54:58 +0900

    session을 위한 인터셉터 구현할 것

---
+++ src/main/java/com/jojoldu/book/webservice/config/interceptor/SessionInterceptor.java
@@ -0,0 +1,6 @@
+package com.jojoldu.book.webservice.config.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class SessionInterceptor implements HandlerInterceptor {
+}
