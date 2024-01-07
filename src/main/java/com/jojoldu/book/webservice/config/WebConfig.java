a8c0127eb38c4a7c99958be30ce971e09fb448ac
KJS <nus0503@naver.com>
2024-01-07 22:09:21 +0900

    세션 관련 공통화을 위해 인터셉터 구현과 해당 클래스 등록

--- src/main/java/com/jojoldu/book/webservice/config/WebConfig.java
+++ src/main/java/com/jojoldu/book/webservice/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.jojoldu.book.webservice.config;
 
 import com.jojoldu.book.webservice.config.auth.LoginUserArgumentResolver;
+import com.jojoldu.book.webservice.config.interceptor.SessionInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,9 +14,18 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer { //스프링에서 인식될 수 있도록
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+    private final SessionInterceptor sessionInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/app/**", "/*.ico", "/error");
     }
 }
--- src/main/java/com/jojoldu/book/webservice/config/interceptor/SessionInterceptor.java
+++ src/main/java/com/jojoldu/book/webservice/config/interceptor/SessionInterceptor.java
@@ -1,6 +1,65 @@
 package com.jojoldu.book.webservice.config.interceptor;
 
+import com.jojoldu.book.webservice.config.auth.PrincipalDetail;
+import com.jojoldu.book.webservice.config.auth.dto.SessionUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+import java.util.UUID;
+@Slf4j
+@Component
 public class SessionInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logID";
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        UUID uuid = UUID.randomUUID();
+        request.setAttribute(LOG_ID, uuid);
+
+        //@RequestMapping: HandlerMethod
+        //정적 리소스: ResourceHttpRequestHandler
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUSET [{}][{}][{}]", uuid, requestURI, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (Objects.nonNull(request.getSession().getAttribute("user"))) {
+            SessionUser user = (SessionUser) request.getSession().getAttribute("user");
+            modelAndView.addObject("username", user.getName());
+        }
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof PrincipalDetail) {
+            PrincipalDetail userInfo = (PrincipalDetail) principal;
+            modelAndView.addObject("userInfo", userInfo.getUser());
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
 }
