273fc9f50211b0f7e746f414baaf857509fc7255
KJS <nus0503@naver.com>
2024-01-18 01:06:34 +0900

    컨트롤러 메서드에서 @ResponseBody를 사용하거나 응답 엔티티를 반환하는 경우 인터셉터 postHandle()이 호출되기 전에 응답을 쓰고 커밋하므로 응답을 변경할 수 없는데 기존 코드는 변경을 일으키므로 코드 수정

--- src/main/java/com/jojoldu/book/webservice/config/interceptor/SessionInterceptor.java
+++ src/main/java/com/jojoldu/book/webservice/config/interceptor/SessionInterceptor.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.webservice.config.interceptor;
 
 import com.jojoldu.book.webservice.config.auth.PrincipalDetail;
 import com.jojoldu.book.webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,13 +14,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Objects;
 import java.util.UUID;
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class SessionInterceptor implements HandlerInterceptor {
 
     public static final String LOG_ID = "logID";
+
+    private final HttpSession httpSession;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
@@ -38,9 +43,18 @@ public class SessionInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        if (Objects.nonNull(request.getSession().getAttribute("user"))) {
-            SessionUser user = (SessionUser) request.getSession().getAttribute("user");
-            modelAndView.addObject("username", user.getName());
+        System.out.println(response.isCommitted());
+//        if (Objects.nonNull(httpSession.getAttribute("user"))) {
+//            SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//            modelAndView.addObject("username", user.getName());
+//        }
+
+        HttpSession session = request.getSession(false); // 현재 세션을 가져옴, 없으면 null 반환
+        if (session != null) {
+            SessionUser user = (SessionUser) session.getAttribute("user");
+            if (user != null) {
+                modelAndView.addObject("username", user.getName());
+            }
         }
 
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
