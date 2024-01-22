11a5d6654d0f6cdbc9c1232eea3cd52b8fb62d3a
kjs <nus0503@naver.com>
2024-01-22 14:47:39 +0900

    업로드한 이미지 다운로드 하기 위한 로직(미완성)

--- src/main/java/com/jojoldu/book/webservice/web/UserViewController.java
+++ src/main/java/com/jojoldu/book/webservice/web/UserViewController.java
@@ -1,18 +1,45 @@
 package com.jojoldu.book.webservice.web;
 
+import com.jojoldu.book.webservice.common.file.FileStore;
 import com.jojoldu.book.webservice.config.auth.LoginUser;
 import com.jojoldu.book.webservice.config.auth.dto.SessionUser;
+import com.jojoldu.book.webservice.domain.userImage.UserImageRepository;
+import com.jojoldu.book.webservice.service.file.FileService;
+import com.jojoldu.book.webservice.service.users.UserService;
+import com.jojoldu.book.webservice.web.dto.UserImageDto;
+import com.nimbusds.oauth2.sdk.util.URLUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.util.UrlUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriUtils;
 
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class UserViewController {
 
+    private final UserService userService;
+
+    private final FileStore fileStore;
+
+    private final FileService fileService;
+
     @GetMapping("/loginForm")
-    public String login(@RequestParam(value = "error", required = false)String error,
-                        @RequestParam(value = "exception", required = false)String exception,
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "exception", required = false) String exception,
                         Model model) {
         model.addAttribute("error", error);
         model.addAttribute("exception", exception);
@@ -28,7 +55,32 @@ public class UserViewController {
     public String modify(@LoginUser SessionUser user, Model model) {
         if (user != null) {
             model.addAttribute("user", user);
+
+            model.addAttribute("image", userService.findById(user.getEmail()));
         }
         return "user-modify";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    @GetMapping("attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        UserImageDto file = fileService.getFile(itemId);
+        String fileName = file.getFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(fileName));
+
+        log.info("uploadFileName={}", fileName);
+
+        String encodedUploadFileName = UriUtils.encode()
+        String contentDisposition = "attachment; filename=\"" + fileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
     }
 }
