811225333798e49cc5714f204496168b71b48066
KJS <nus0503@naver.com>
2024-01-22 18:55:24 +0900

    서버 측 파일 확장자 유효성 검사를 위한 enum클래스와 어노테이션(미완성)

---
+++ src/main/java/com/jojoldu/book/webservice/common/validation/file/FileUploadValid.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.webservice.common.validation.file;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FileUploadValidator.class)
+public @interface FileUploadValid {
+
+
+}
---
+++ src/main/java/com/jojoldu/book/webservice/common/validation/file/UploadAllowFileDefine.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.webservice.common.validation.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 업로드 허용 파일에 대한 정의 Enum
+ */
+@Getter
+@AllArgsConstructor
+public enum UploadAllowFileDefine {
+
+    CSV("csv", new String[]{"image/jpg", "image/png"});
+
+    private String fileExtensionLowerCase; // 파일 확장자(소문자)
+    private String[] allowMimeTypes; // 허용되는 mime type array(파일 내용 변조 후 확장자를 변경하는 공격을 막기 위해서 사용
+}
