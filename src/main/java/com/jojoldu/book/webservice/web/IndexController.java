d467701be7188ec9fb7a6587b9a584eb85bceeb9
KJS <nus0503@naver.com>
2024-01-26 20:09:16 +0900

    클라이언트 ip를 가져오기 위한 로직(미완) * IPv6이라서 그런지 ip가 출력되지 않는다. 추가 검색하고 완성해야함

--- src/main/java/com/jojoldu/book/webservice/web/IndexController.java
+++ src/main/java/com/jojoldu/book/webservice/web/IndexController.java
@@ -96,4 +96,32 @@ public class IndexController {
         @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         private LocalDateTime localDateTime;
     }
+
+    @GetMapping("/ip")
+    public void getIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        log.info("X-FORWARDED-FOR : " + ip);
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("HTTP_CLIENT_IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("HTTP_X_FORWARDED_FOR : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+            log.info("getRemoteAddr : "+ip);
+        }
+        log.info("Result : IP Address : "+ip);
+    }
 }
