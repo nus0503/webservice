4d6faac4ca46b934ba086b5af444be21845e3e37
kjs <nus0503@naver.com>
2024-02-26 01:12:33 +0900

    토큰 관련 유틸클래스 메서드 작성

--- src/main/java/com/jojoldu/book/webservice/common/jwt/TokenUtil.java
+++ src/main/java/com/jojoldu/book/webservice/common/jwt/TokenUtil.java
@@ -2,14 +2,17 @@ package com.jojoldu.book.webservice.common.jwt;
 
 import com.jojoldu.book.webservice.controller.user.dto.UserFindPasswordDto;
 import com.jojoldu.book.webservice.domain.oAuthUser.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,15 +35,100 @@ public class TokenUtil {
         jwtSecretKey = value;
     }
 
+    /**
+     * 사용자 정보 기반으로 토큰 생성 및 반환
+     *
+     * @param dto : 사용자가 비밀번호를 찾기 위한 정보들
+     * @param User : 실제 DB 유저 정보
+     * @return 토큰
+     */
+
     public static String generateJwtToken(UserFindPasswordDto dto, User User) {
 
-        Jwts.builder()
-                .setHeader(createHeader())
-                .setClaims(createClaims(dto))
-                .setSubject(String.valueOf(User.getId()))
-                .signWith()
+        return Jwts.builder()
+                .setHeader(createHeader()) // Header 구성
+                .setClaims(createClaims(dto)) // Payload - Claims 구성
+                .setSubject(String.valueOf(User.getId())) // Payload - Subject 구성
+                .signWith(SignatureAlgorithm.HS256, createSignature()) // Signature 구성
+                .setExpiration(createExpiredDate()) // Expired Date 구성
+                .compact();
     }
 
+    /**
+     * 토큰 기반으로 사용자 정보 반환
+     * @param token
+     * @return
+     */
+    public static String parseTokenToUserInfo(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    /**
+     * 유효한 토큰인지 확인
+     * @param token : 토큰
+     * @return
+     */
+    public static boolean isValidToken(String token) {
+        try {
+            Claims claims = getClaimsFormToken(token);
+
+            log.info("expireTime : " + claims.getExpiration());
+            log.info("userName : " + claims.get("username"));
+            log.info("userEmail : " + claims.get("userEmail"));
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("Token Expired");
+            return false;
+        } catch (JwtException e) {
+            log.error("Token Tampered");
+            return false;
+        } catch (NullPointerException e) {
+            log.error("Token is null");
+            return false;
+        }
+    }
+
+    /**
+     * Header 내에서 토큰 추출
+     * @param header
+     * @return
+     */
+    public static String getTokenFromHeader(String header) {
+        return header.split(" ")[1];
+    }
+
+    /**
+     * 토큰 정보를 기반으로 Claims 정보를 반환하는 메서드
+     * @param token
+     * @return
+     */
+    private static Claims getClaimsFormToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(jwtSecretKey))
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * 토큰의 만료기간 지정 메소드
+     * @return
+     */
+    private static Date createExpiredDate() {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MINUTE, 30);
+        return c.getTime();
+    }
+
+    /**
+     * 사용자 정보 기반으로 클래임을 생성
+     * @param dto
+     * @return
+     */
     private static Map<String, Object> createClaims(UserFindPasswordDto dto) {
 
         HashMap<String, Object> claims = new HashMap<>();
@@ -54,6 +142,10 @@ public class TokenUtil {
         return claims;
     }
 
+    /**
+     * JWT "헤더"값을 생성
+     * @return
+     */
     private static Map<String, Object> createHeader() {
 
         HashMap<String, Object> header = new HashMap<>();
@@ -64,7 +156,12 @@ public class TokenUtil {
         return header;
     }
 
+    /**
+     * JWT "서명(Signature)"을 발급하는 메소드
+     * @return
+     */
     private static Key createSignature() {
-
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(jwtSecretKey);
+        return new SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.getJcaName());
     }
 }
